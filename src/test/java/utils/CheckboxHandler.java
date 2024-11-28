fedea3960aa2ad9e643b461d9a04f26dba53d0fc
Peter Surka <peter.surka@gmail.com>
2024-11-28 17:08:08 +0100

    checkboxHandler update

--- src/test/java/utils/CheckboxHandler.java
+++ src/test/java/utils/CheckboxHandler.java
@@ -6,13 +6,10 @@ import org.openqa.selenium.WebElement;
 
 public class CheckboxHandler {
 
-    // Method to check the checkbox using WebDriver
     public static void checkCheckbox(WebDriver driver, String xpath) {
-        // Locate the checkbox using the provided XPath
         WebElement checkbox = driver.findElement(By.xpath(xpath));
-        // Check if the checkbox is not already selected
         if (!checkbox.isSelected()) {
-            checkbox.click(); // Click the checkbox to select it
+            checkbox.click();
         }
     }
 }
